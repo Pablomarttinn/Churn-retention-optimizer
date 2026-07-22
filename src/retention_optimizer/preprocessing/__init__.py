285ddcbc1e86283e52161930e25217c4996155bb
Pablo <pablomrtfdez@gmail.com>
2026-07-22 16:32:59 +0200

    Session 2 finished

--- src/retention_optimizer/preprocessing/__init__.py
+++ src/retention_optimizer/preprocessing/__init__.py
@@ -1,5 +1,6 @@
-"""Preprocessing package: data cleaning and feature engineering pipelines."""
+"""Preprocessing package: cleaning, feature engineering, encoding and split."""
 
+from .pipelines.encoding import encode, scale_numeric, split_data
 from .preprocessing import preprocess
 
-__all__ = ["preprocess"]
+__all__ = ["preprocess", "encode", "split_data", "scale_numeric"]
